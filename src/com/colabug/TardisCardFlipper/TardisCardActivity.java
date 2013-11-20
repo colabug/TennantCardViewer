fdc12e990e960147d7340e8dacc6c9ccfd142b64
Corey Leigh Latislaw <colabug@gmail.com>
2013-11-20 13:11:55 -0500

    Added images and cards for them, updated text

--- src/com/colabug/TardisCardFlipper/TardisCardActivity.java
+++ src/com/colabug/TardisCardFlipper/TardisCardActivity.java
@@ -11,6 +11,14 @@ import java.util.List;
 public class TardisCardActivity extends Activity
 {
     private List<Card> cards;
+    private int[] imageIds = { R.drawable.tennant_glasses,
+                               R.drawable.tenannt_longing,
+                               R.drawable.space_tennant,
+                               R.drawable.tennant_snow,
+                               R.drawable.tennant_bars,
+                               R.drawable.clock_tennant,
+                               R.drawable.king_tennant
+                             };
 
     @Override
     public void onCreate( Bundle savedInstanceState )
@@ -24,19 +32,21 @@ public class TardisCardActivity extends Activity
     private void createCardList()
     {
         cards = new ArrayList<Card>();
-        cards.add( generateCard() );
-        cards.add( generateCard() );
-        cards.add( generateCard() );
+
+        for ( int imageId : imageIds )
+        {
+            cards.add( generateCard( imageId ) );
+        }
     }
 
-    private Card generateCard()
+    private Card generateCard( int imageId )
     {
         Card card = new Card( this );
 
         card.setText( getString( R.string.CARD_TITLE ) );
         card.setInfo( getString( R.string.CARD_SUBTITLE ) );
         card.setFullScreenImages( true );
-        card.addImage( R.drawable.tennant_glasses );
+        card.addImage( imageId );
 
         return card;
     }
