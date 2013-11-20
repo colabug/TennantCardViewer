18d4e1ad79361d1fe638086038009ab00233ffc0
Corey Leigh Latislaw <colabug@gmail.com>
2013-11-20 11:12:37 -0500

    Hello glass

---
+++ src/com/colabug/TardisCardFlipper/TardisCardActivity.java
@@ -0,0 +1,17 @@
+package com.colabug.TardisCardFlipper;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TardisCardActivity extends Activity
+{
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.main );
+    }
+}
