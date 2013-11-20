196d1186a63a159ac9e15ba759000623aa9d91b8
Corey Leigh Latislaw <colabug@gmail.com>
2013-11-20 11:42:26 -0500

    Adding a card

--- src/com/colabug/TardisCardFlipper/TardisCardActivity.java
+++ src/com/colabug/TardisCardFlipper/TardisCardActivity.java
@@ -2,16 +2,22 @@ package com.colabug.TardisCardFlipper;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import com.google.android.glass.app.Card;
 
 public class TardisCardActivity extends Activity
 {
-    /**
-     * Called when the activity is first created.
-     */
     @Override
     public void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.main );
+
+        Card card1 = new Card( this );
+        card1.setText( getString( R.string.CARD_TITLE ) );
+        card1.setInfo( getString( R.string.CARD_SUBTITLE ) );
+        card1.setFullScreenImages( true );
+        card1.addImage( R.drawable.tennant_glasses );
+        View card1View = card1.toView();
+        setContentView( card1View );
     }
 }
