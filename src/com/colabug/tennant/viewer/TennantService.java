89c83dad853f932600feab578ff6db12a4166069
Corey Leigh Latislaw <colabug@gmail.com>
2013-12-02 13:13:34 -0500

    REFACTOR: Added license information and abstracted fucntions

--- src/com/colabug/tennant/viewer/TennantService.java
+++ src/com/colabug/tennant/viewer/TennantService.java
@@ -1,4 +1,20 @@
 package com.colabug.tennant.viewer;
+/*
+ * Copyright (C) 2013 The Android Open Source Project and
+ * modified by Corey Leigh Latislaw
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import android.app.PendingIntent;
 import android.app.Service;
@@ -25,6 +41,8 @@ public class TennantService extends Service
     /**
      * A binder that gives other components access to the speech capabilities provided by the
      * service.
+     *
+     * TODO: Use elsewhere or remove
      */
     public class TennantBinder extends Binder
     {
@@ -69,25 +87,38 @@ public class TennantService extends Service
     {
         if ( liveCard == null )
         {
-            liveCard = timelineManager.getLiveCard( LIVE_CARD_ID );
-            tennantRenderer = new TennantRenderer( this );
-
-            liveCard.enableDirectRendering( true )
-                    .getSurfaceHolder()
-                    .addCallback( tennantRenderer );
-            liveCard.setNonSilent( true );
-
-            // Display the options menu when the live card is tapped.
-            Intent menuIntent = new Intent( this, TennantMenuActivity.class );
-            menuIntent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK );
-            liveCard.setAction( PendingIntent.getActivity( this,
-                                                           0,
-                                                           menuIntent,
-                                                           0 ) );
+            configureLiveCard();
             liveCard.publish();
         }
 
         return START_STICKY;
+    }
+
+    /**
+     * Configures live card.
+     */
+    private void configureLiveCard()
+    {
+        liveCard = timelineManager.getLiveCard( LIVE_CARD_ID );
+        tennantRenderer = new TennantRenderer( this );
+
+        liveCard.enableDirectRendering( true )
+                .getSurfaceHolder()
+                .addCallback( tennantRenderer );
+        liveCard.setNonSilent( true );
+
+        configureMenu();
+    }
+
+    /**
+     * Configures display of options menu when the live card is tapped.
+     */
+    private void configureMenu()
+    {
+        Intent menuIntent = new Intent( this, TennantMenuActivity.class );
+        menuIntent.addFlags( Intent.FLAG_ACTIVITY_NEW_TASK |
+                             Intent.FLAG_ACTIVITY_CLEAR_TASK );
+        liveCard.setAction( PendingIntent.getActivity( this, 0, menuIntent, 0 ) );
     }
 
     @Override
